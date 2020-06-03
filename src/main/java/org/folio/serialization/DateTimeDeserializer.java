881a6b5427c1efd043d45d33cef4c6aca22b7c47
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2020-06-03 18:48:17 +0300

    [MODPATBLK-7] fixed code smell

--- src/main/java/org/folio/serialization/DateTimeDeserializer.java
+++ src/main/java/org/folio/serialization/DateTimeDeserializer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.joda.time.DateTime;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
