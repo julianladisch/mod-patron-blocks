55a8df2b701f14aabb0865f37aa0fd570a57fb3d
Alexander Kurash <Oleksandr_Kurash@epam.com>
2020-06-09 22:27:17 +0300

    MODPATBLK-11 Add test cases

--- src/main/java/org/folio/rest/impl/EventHandlersAPI.java
+++ src/main/java/org/folio/rest/impl/EventHandlersAPI.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.ws.rs.core.Response;
 
 import org.folio.domain.EventType;
+import org.folio.rest.handlers.ItemCheckedOutEventHandler;
 import org.folio.rest.jaxrs.model.FeeFineBalanceChangedEvent;
 import org.folio.rest.jaxrs.model.ItemCheckedInEvent;
 import org.folio.rest.jaxrs.model.ItemCheckedOutEvent;
@@ -48,7 +49,7 @@ public class EventHandlersAPI implements AutomatedPatronBlocksHandlers {
     asyncResultHandler.handle(Future.succeededFuture(
       PostAutomatedPatronBlocksHandlersItemCheckedOutResponse.respond204()));
 
-    logEventReceived(event);
+    new ItemCheckedOutEventHandler(okapiHeaders, vertxContext.owner()).handle(event);
   }
 
   @Override
