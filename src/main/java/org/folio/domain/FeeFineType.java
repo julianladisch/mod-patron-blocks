daed243dcd54a1535971b3f33c79fcc93ffcc939
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2020-07-02 12:36:01 +0300

    [MODPATBLK-21] Removed unused fields and methods from FeeFineType to increase coverage

--- src/main/java/org/folio/domain/FeeFineType.java
+++ src/main/java/org/folio/domain/FeeFineType.java
@@ -3,27 +3,15 @@ package org.folio.domain;
 public enum FeeFineType {
   // These are hardcoded in mod-feesfines
 
-  OVERDUE_FINE("Overdue fine", "9523cb96-e752-40c2-89da-60f3961a488d", true),
-  REPLACEMENT_PROCESSING_FEE("Replacement processing fee", "d20df2fb-45fd-4184-b238-0d25747ffdd9", true),
-  LOST_ITEM_FEE("Lost item fee", "cf238f9f-7018-47b7-b815-bb2db798e19f", true),
-  LOST_ITEM_PROCESSING_FEE("Lost item processing fee", "c7dede15-aa48-45ed-860b-f996540180e0", true);
+  OVERDUE_FINE("9523cb96-e752-40c2-89da-60f3961a488d"),
+  REPLACEMENT_PROCESSING_FEE("d20df2fb-45fd-4184-b238-0d25747ffdd9"),
+  LOST_ITEM_FEE("cf238f9f-7018-47b7-b815-bb2db798e19f"),
+  LOST_ITEM_PROCESSING_FEE("c7dede15-aa48-45ed-860b-f996540180e0");
 
-  private final String name;
   private final String id;
-  private final boolean automatic;
 
-  FeeFineType(String name, String id, boolean automatic) {
-    this.name = name;
+  FeeFineType(String id) {
     this.id = id;
-    this.automatic = automatic;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public boolean isAutomatic() {
-    return automatic;
   }
 
   public String getId() {
