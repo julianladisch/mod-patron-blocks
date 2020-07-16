c86141a6cba83d77dee557daaace28b1bdaad872
Roman_Barannyk <roman_barannyk@epam.com>
2020-07-16 19:42:16 +0300

    MODPATBLK-36 Remove unnecessary fields from DB
    (cherry picked from commit 15b7a388642582df10eeb466c90e6de271521089)

---
+++ src/main/resources/templates/db_scripts/remove-user_summary-v1.0.4-unnecessary-fields.sql
@@ -0,0 +1,7 @@
+UPDATE ${myuniversity}_${mymodule}.user_summary
+SET jsonb = jsonb - 'numberOfLostItems'
+WHERE jsonb->'numberOfLostItems' IS NOT NULL;
+
+UPDATE ${myuniversity}_${mymodule}.user_summary
+SET jsonb = jsonb - 'outstandingFeeFineBalance'
+WHERE jsonb->'outstandingFeeFineBalance' IS NOT NULL;
